aa4f35178c76ceee6c50016e50f0efae19825496
Yusuf Edmond <spoonfulltaken@gmail.com>
2023-09-23 19:21:38 -0400

    front end started

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import Header from './components/Header';
+import HomeScreen from './Screens/HomeScreen';
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <HomeScreen />
+    </>
+  );
+}
+
+export default App;
