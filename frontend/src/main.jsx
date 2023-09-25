e4ae49b7877f4d72f04a41148f347b783382fe42
Yusuf Edmond <spoonfulltaken@gmail.com>
2023-09-24 22:14:11 -0400

    fixes

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,20 +6,23 @@ import {
   Route,
   RouterProvider} 
   from 'react-router-dom'
+  import App from './App.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './index.css'
 import HomeScreen from './Screens/HomeScreen.jsx'
+import LoginScreen from './Screens/LoginScreen.jsx'
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path="/" element={<App />} >
-      <Route path='/' element={<HomeScreen />} />
+    <Route path='/' element={<App />} >
+      <Route index={true} path='/' element={<HomeScreen />} />
+      <Route path='/login' element={<LoginScreen />} />
     </Route>
   )
 )
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={ router } />
   </React.StrictMode>,
 )
